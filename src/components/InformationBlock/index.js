ee70acc4d45651e0ed971fa21f2400ff2497aab1
vaibhav993 <vaibhavbotre2@gmail.com>
2020-03-30 01:52:14 +0530

    fixed router issue

--- src/components/InformationBlock/index.js
+++ src/components/InformationBlock/index.js
@@ -3,11 +3,12 @@ import React, { memo } from "react";
 import "./index.scss";
 
 const InformationBlock = memo(({ title, delta, count }) => {
+    console.log(process.env.PUBLIC_URL);
     return (
         <div className="information-block col-lg-3 col-md-3 col-6">
             <div className={"level-item ".concat(title)}>
                 <h5>{title}</h5>
-                <img src={`./img/${title}.png`} alt={title}/>
+                <img src={`${process.env.PUBLIC_URL}/img/${title}.png`} alt={title}/>
                 <h4>{delta ? `[+${delta}]` : '\u00A0'}</h4>
                 <h1>{count}</h1>
             </div>
